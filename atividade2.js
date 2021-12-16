c27fa08db55646505b828d12589318c178b18914
Flavio Coutinho <fegemo@gmail.com>
2021-12-16 18:54:13 -0300

    Activity 2: paragraphs

--- atividade2.js
+++ atividade2.js
@@ -1,6 +1,13 @@
 // Faça o exercício DOS PARÁGRAFOS neste arquivo
 // Este arquivo AINDA NÃO está incluído no arquivo HTML
 
+let botoes = document.querySelectorAll('.botao-expandir-retrair');
 
+for (let botaoEl of botoes) {
+  botaoEl.addEventListener('click', function(e) {
+    let colocou = e.currentTarget.parentNode.classList.toggle('expandido');
+    e.currentTarget.innerHTML = colocou ? '-' : '+';
+  });
+}
 
 
